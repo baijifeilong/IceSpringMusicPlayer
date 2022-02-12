1b39dd8bf9f997dfec74ab3d5bf25bc60921c73b
BaiJiFeiLong <baijifeilong@gmail.com>
2022-02-12 23:06:27 +0800

    Add smooth spectrum support

--- IceSpringMusicPlayer/plugins/IceSpringSpectrumPlugin/spectrumWidget.py
+++ IceSpringMusicPlayer/plugins/IceSpringSpectrumPlugin/spectrumWidget.py
@@ -2,6 +2,7 @@
 import collections
 import logging
 import math
+import typing
 
 import numpy as np
 from PySide2 import QtWidgets, QtGui, QtCore
@@ -19,7 +20,7 @@ class SpectrumWidget(QtWidgets.QWidget, PluginWidgetMixin):
         self._player = App.instance().getPlayer()
         self._timer = QtCore.QTimer(self)
         self._timer.timeout.connect(self._onTimeout)
-        self._timer.start(50)
+        self._timer.start(40)
 
     def _onTimeout(self):
         self._logger.debug("On timeout, repaint")
@@ -31,6 +32,16 @@ class SpectrumWidget(QtWidgets.QWidget, PluginWidgetMixin):
             self._logger.debug("Report duration: %d, real duration: %d", reportedDuration, realDuration)
         self.repaint()
 
+    def smoothArray(self, arr: typing.List[float]):
+        up, down = 0.9, 0.9
+        oldArr = getattr(self, "__arr", [])
+        for i in range(len(oldArr)):
+            if arr[i] < oldArr[i]:
+                arr[i] = arr[i] * down + oldArr[i] * (1 - down)
+            else:
+                arr[i] = arr[i] * up + oldArr[i] * (1 - up)
+        setattr(self, "__arr", arr)
+
     def paintEvent(self, event: QtGui.QPaintEvent) -> None:
         music = self._player.getCurrentMusic()
         samples = self._player.getSamples()
@@ -41,12 +52,11 @@ class SpectrumWidget(QtWidgets.QWidget, PluginWidgetMixin):
             self._logger.debug("No samples, skip")
             return
         sampleRate = music.get().sampleRate
-        baseFrequency, maxFrequency, barCount, minDbfs, sampleCount = 50, 22000, 20, -60, 1500
+        baseFrequency, maxFrequency, barCount, minDbfs, sampleCount = 50, 20000, 30, -60, 1500
         powerRoot = pow(maxFrequency / baseFrequency, 1 / barCount)
         sampleIndex = int(self._player.getPosition() / 1000 * sampleRate)
         segments = samples[sampleIndex:sampleIndex + sampleCount]
-        frequencies, powers = signal.welch(segments, fs=sampleRate, nperseg=sampleCount, scaling="spectrum",
-            noverlap=750)
+        frequencies, powers = signal.welch(segments, fs=sampleRate, nperseg=sampleCount, scaling="spectrum")
         powers[powers == 0] = 2 ** -40
         powers = np.log10(powers * 2) * 10
         spacing = self.width() // barCount
@@ -55,8 +65,9 @@ class SpectrumWidget(QtWidgets.QWidget, PluginWidgetMixin):
         for frequency, power in zip(frequencies, powers):
             valuesIndex = 0 if frequency == 0 else int(math.log(frequency / baseFrequency, powerRoot))
             valuesDict[min(max(0, valuesIndex), barCount - 1)].append(power)
-        for valuesIndex, values in valuesDict.items():
-            value = -sum(values) / len(values)
+        values = [-sum(x) / len(x) for x in valuesDict.values()]
+        self.smoothArray(values)
+        for valuesIndex, value in enumerate(values):
             unit = self.height() / -minDbfs
             painter.fillRect(valuesIndex * spacing, int(value * unit), spacing - 1, self.height(),
                 QtGui.QColor("#4477CC"))
